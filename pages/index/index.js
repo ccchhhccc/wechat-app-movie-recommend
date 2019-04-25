becf362c8a5f6059b92ee82c8b62aa4e816c7ca5
chenhaichao <1428524567@qq.com>
2019-04-25 20:40:02 +0800

    新增搜索页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -125,6 +125,11 @@ Page({
       url: '/pages/detail/detail?movieId=' + e.currentTarget.dataset.movieid,
     })
   },
+  gotoSearch(){
+    wx.navigateTo({
+      url: '/pages/search/search'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
---
+++ pages/search/search.js
@@ -0,0 +1,93 @@
+// pages/search/search.js
+
+import { http, delUrl } from '../../utils/util.js'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info:'',
+    list:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  change(e){
+    this.setData({
+      info: e.detail.value
+    })
+  },
+  search(){
+    wx.showLoading({
+      title: '玩命加载中',
+      mask: false
+    })
+    let url = 'https://movie.douban.com/j/subject_suggest?q=' + this.data.info
+    http('/toApi', url).then((rel) => {
+      console.log(rel)
+      wx.hideLoading()
+      this.setData({
+        list:rel
+      })
+    })
+  },
+  goToDetail(e) {
+    wx.navigateTo({
+      url: '/pages/detail/detail?movieId=' + e.currentTarget.dataset.movieid,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
